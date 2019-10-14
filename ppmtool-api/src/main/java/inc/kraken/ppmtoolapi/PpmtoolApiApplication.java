1eda1dbcb8a163b15a9ff32b9a1e26440f1f48bd
Carlos Ndiaye <carlosndiay@gmail.com>
2019-10-14 14:51:39 +0000

    First commit

---
+++ ppmtool-api/src/main/java/inc/kraken/ppmtoolapi/PpmtoolApiApplication.java
@@ -0,0 +1,13 @@
+package inc.kraken.ppmtoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApiApplication.class, args);
+    }
+
+}
